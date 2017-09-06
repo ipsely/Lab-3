8db5864d5dfef3168f01b817c0ca63bd9f1307af
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-09-06 13:16:47 +0530

    Create Problem 12.cpp

---
+++ Problem 12.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int month;
+   
+    cout << "Enter month number (1-12): ";
+    cin >> month;
+    if (month == 4 || month == 6 || month == 9 || month == 11)
+    {
+      cout << month <<" month has 30 days.";
+    }
+		
+    else if (month == 02)
+    {
+      cout << month <<" month has 28 days or 29 days if it's a leap year.";		
+    }
+
+    else 
+    {
+      cout << month <<" month has 31 days.";
+    }  
+
+    return 0;
+}
