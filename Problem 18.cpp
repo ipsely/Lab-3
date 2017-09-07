1259e1b47f774591a0498f9806cfe1efe270e393
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-09-07 23:10:54 +0530

    Create Problem 18.cpp

---
+++ Problem 18.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int cp,sp,result;
+
+	cout<<"Enter cost price of item : "<<endl;
+	cin>>cp;
+	cout<<"Enter selling price of item : "<<endl;
+	cin>>sp;
+
+	result=sp-cp;
+
+	if(result>0)
+	{
+		cout<<"Profit : "<<result;
+	}
+	else if(result<0)
+	{
+    			cout<<"Loss : "<<-(result);
+	}
+	else
+	{
+		cout<<"No profit, no loss.";
+            }
+
+	return 0;
+}
