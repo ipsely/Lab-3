da73222a6841b5405ff263fc65483a62ef6f67a1
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-09-09 00:08:50 +0530

    Create Problem 19.cpp

---
+++ Problem 19.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int maths, physics, biology, chemistry, computer, sum, percentage;
+	cout << "Enter marks obtained in the five subjects :"<<endl;
+	cin >>maths>>physics>>biology>>chemistry>>computer;
+	sum = maths+physics+biology+chemistry+computer;
+	percentage = sum / 5;
+	if (percentage >= 90)
+	{
+		cout << "Grade A";
+	}
+	else if (percentage >= 80)
+	{
+		cout << "Grade B";
+	}
+	else if (percentage >= 70)
+	{
+		cout << "Grade C";
+	}
+	else if (percentage >= 60)
+	{
+		cout << "Grade D";
+	}
+	else if (percentage >= 40)
+	{
+		cout << "Grade E";
+	}
+	else if (percentage < 40)
+	{
+		cout << "Grade F";
+	}
+	else
+	{
+		cout << "Check the entered data again.";
+	}
+	return 0;
+}
