738f66b1014bac2190b69f8cab1ca8ec44db2649
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-09-10 01:01:18 +0530

    Create Problem 25.cpp

---
+++ Problem 25.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+            int i,n;
+            i=1;
+	while(i<100)
+	{
+		if(i%2==0)
+		{
+		cout<<i<<endl;
+		}
+		i++;
+	}
+	return 0;
+}
