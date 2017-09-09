e20acb634212c6f366c277bc000315116fe66fed
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-09-10 00:53:49 +0530

    Create Problem 23.cpp

---
+++ Problem 23.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout<<"Print all natural numbers from n to 1. Enter n : "<<endl;
+	cin>>n;
+	
+	while(n>=1)
+	{
+		cout<<n<<endl;
+		n--;
+	}
+	return 0;
+}
