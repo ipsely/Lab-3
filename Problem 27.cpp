e241ba69da293598676ff1b8d8bef5ac4a113696
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-09-10 01:09:58 +0530

    Create Problem 27.cpp

---
+++ Problem 27.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+            int i,n;
+            int sum=0;
+            cout<<"Sum of all natural numbers between 1 to : "<<endl;
+            cin>>n;
+            i=1;
+	while(i<n)
+	{
+		sum=sum+i;
+		i++;
+	}
+	cout<<"Sum is "<<sum;
+	return 0;
+}
