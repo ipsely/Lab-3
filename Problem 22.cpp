322eeaaabb04fb586a9a59b74dc0b96ada52f705
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-09-10 00:42:07 +0530

    Create Problem 22.cpp

---
+++ Problem 22.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,n;
+	cout<<"Print all natural numbers from 1 to : "<<endl;
+	cin>>n;
+	i=1;
+	while(i<=n)
+	{
+		cout<<i<<endl;
+		i++;
+	}
+	return 0;
+}
