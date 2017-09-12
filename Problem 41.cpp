d823539acfe5553cb457a90c51613c6826046d42
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-09-13 01:27:09 +0530

    Create Problem 41.cpp

---
+++ Problem 41.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    int i;
+    cout<<"Values"<<"    "<<"ASCII CODES"<<endl;
+    
+    for (i=0;i<=225;i++)
+    {
+    	a=(char) i;
+    	cout<<i<<"          "<<a<<endl;
+    }
+    return 0;
+}
