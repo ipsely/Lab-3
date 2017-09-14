ebb39fececcaf7e4da9e94eb750f349fd55164e3
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-09-14 23:15:11 +0530

    Update Problem 47.cpp

--- Problem 47.cpp
+++ Problem 47.cpp
@@ -2,21 +2,25 @@
 using namespace std;
    
 int main() {
-	int i,n;
+	int i,n,f=0;
 	cout<<"Enter any number :"<<endl;
 	cin>>n;
-	for(i=2;i<=n/2;i++)
+	for(i=2;i<n;i++)
 	{
 		if(n%i==0)
 		{
-			cout<<n<<" is not a Prime Number";
+			f=1;
 			break;
 		}
-		else
-		{
-			cout<<n<<" is a Prime Number";
-			break;
-		}
+		
 	}
-}      
+	if(f==0)
+	{
+	            cout<<n<<" is a Prime Number";
+	}
+	else
+	{
+		cout<<n<<" is not a Prime Number";
+	}
+}          
             
