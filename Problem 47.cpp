cee959af8ed6d6559a5639d74fc096a4583aee2d
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-09-14 22:41:21 +0530

    Create Problem 47.cpp

---
+++ Problem 47.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+   
+int main() {
+	int i,n;
+	cout<<"Enter any number :"<<endl;
+	cin>>n;
+	for(i=2;i<=n/2;i++)
+	{
+		if(n%i==0)
+		{
+			cout<<n<<" is not a Prime Number";
+			break;
+		}
+		else
+		{
+			cout<<n<<" is a Prime Number";
+			break;
+		}
+	}
+}      
+            
